e59f3165401bd36cd3ef3d212ea349cc6a04b636
elizabethhillman <elizabethhillman0@gmail.com>
2022-09-29 13:39:45 -0700

    changed prints to returns

--- hw2/ScholarshipTest.java
+++ hw2/ScholarshipTest.java
@@ -1,19 +1,4 @@
-package hw2;
-
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-
 class ScholarshipTest {
-
-    @Test
-    void main() {
-    }
-
-    @Test
-    void checkEligibility() {
-    }
-
-    @Test
-    void charToBool() {
-    }
+  
 }
